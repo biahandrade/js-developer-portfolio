9d604559ed02a628eb5ff74219aefeab6c5dd8b3
Bianca Andrade <166971835+biahandrade@users.noreply.github.com>
2025-07-06 03:07:48 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -5,14 +5,4 @@ async function fetchProfileData() {
     const profileData = await response.json()
     return profileData
 }
-fetch(url)
-  .then(response => {
-    if (!response.ok) throw new Error("Erro ao buscar JSON");
-    return response.json();
-  })
-  .then(data => {
-    // mostrar dados
-  })
-  .catch(error => {
-    console.error("Erro ao carregar dados:", error);
-  });
+
